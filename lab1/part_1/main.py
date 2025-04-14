0e5f9985030c033c47b011b2033b8fdf12896a77
DANILA21-3 <cherdanila184@gmail.com>
2025-04-14 18:11:52 +0400

    Update main.py
    Использовано try/except вместо if/else
    
    Импортирован модуль для чтения/записи файлов

--- lab1/part_1/main.py
+++ lab1/part_1/main.py
@@ -1,41 +1,4 @@
-import json
-from typing import Union
-
-def read_file(file_path: str, file_type: str = 'json') -> Union[dict, str]:
-    """
-    Читает файл и возвращает его содержимое.
-
-    :param file_path: Путь к файлу
-    :param file_type: Тип файла (txt или json)
-    :return: Содержимое файла
-    """
-    try:
-        match file_type:
-            case 'json':
-                with open(file_path, 'r', encoding='utf-8') as file:
-                    return json.load(file)
-            case 'text':
-                with open(file_path, 'r', encoding='utf-8') as file:
-                    return file.read()
-            case _:
-                raise ValueError("Неподдерживаемый формат файла")
-    except Exception as e:
-        print(f"Ошибка при чтении файла {file_path}: {e}")
-        return {} if file_type == 'json' else ""
-    
-
-def write_to_file(file_path: str, text: str):
-    """
-    Фунция записывает строковую переменную в текстовый файл
-
-    :param file_path: Путь, в котором будет храниться файл
-    :param text: Записываемый текст 
-    """
-    try:
-        with open(file_path, 'w', encoding='utf-8') as file:
-            file.write(text)
-    except Exception as e:
-        print(f"Ошибка при записи в файл: {e}")
+from ..modules.read_write_function import read_file, write_to_file
 
 def crypt(text: str, replacement_dict: dict):
     """
@@ -81,7 +44,7 @@ def main():
     text = read_file(config['input_file_path'], 'text')
     replacement_dict = read_file(config['requirements_file_path'], 'json')
 
-    if text:
+    try:
 
         encrypted_text = crypt(text, replacement_dict)
         write_to_file(config['encrypted_text_output_file_path'], encrypted_text)
@@ -91,8 +54,8 @@ def main():
         write_to_file(config['decrypted_text_output_file_path'], decrypted_text)
         print("Расшифрованный текст успешно записан")
 
-    else:
-        print("Текст для шифрования пуст")
+    except Exception as error:
+        print(f"Произошла ошибка: {error}")
 
 if __name__ == "__main__":
     main()

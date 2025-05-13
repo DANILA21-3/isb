18736d688b87d4f7e6b029d4a867522e1af0b92a
DANILA21-3 <cherdanila184@gmail.com>
2025-05-13 15:08:03 +0400

    Update generator.java

--- lab2/generator.java
+++ lab2/generator.java
@@ -2,13 +2,27 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
 
-public class generator{
+/**
+ * Класс для генерации псевдослучайной бинарной последовательности
+ * и записи её в файл
+ */
+public class generator { 
+    
+    /**
+     * Метод класса создаёт объект для генерации случайной последовательности, которая в итоге
+     * преобразуется в строковый формат. Полученный результат выводится на экран и записывается в файл
+     * 
+     * @param args аргументы командной строки (не используются, но необходимо для работы метода класса)
+     * 
+     * @throws IOException вызывается в случае появления ошибки при записи файла
+     */
     public static void main(String[] args) {
         Random random = new Random();
+        
         StringBuilder binarySequence = new StringBuilder(128);
 
         for (int i = 0; i < 128; i++) {
-            binarySequence.append(random.nextInt(2));
+            binarySequence.append(random.nextInt(2)); 
         }
 
         String sequence = binarySequence.toString();
@@ -16,7 +30,7 @@ public class generator{
 
         try (FileWriter fileWriter = new FileWriter("sequence_java.txt")) {
             fileWriter.write(sequence);
-            System.out.println("Последовательность успешно записана в файл random_binary_sequence.txt");
+            System.out.println("Последовательность успешно записана в файл sequence_java.txt");
         } catch (IOException e) {
             System.err.println("Ошибка при записи в файл: " + e.getMessage());
         }

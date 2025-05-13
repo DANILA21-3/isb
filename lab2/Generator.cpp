ae418075cdbbdc57d688f1a775c17c63d2458268
DANILA21-3 <cherdanila184@gmail.com>
2025-05-13 15:07:38 +0400

    Update Generator.cpp

--- lab2/Generator.cpp
+++ lab2/Generator.cpp
@@ -2,7 +2,14 @@
 #include <random>
 #include <fstream>
 #include <bitset>
-
+/*
+  Генерирует бинарной псевдослучайную последовательность и сохраняет её в файл
+  
+  В случае неуспешного сохранения в файл вызовет исключение
+  
+  Параметры:
+  filename - путь для сохранения файла
+*/
 void generateAndSaveSequence(const std::string& filename) {
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -21,7 +28,11 @@ void generateAndSaveSequence(const std::string& filename) {
     outFile << bits.to_string() << std::endl; 
     outFile.close();
 }
+/*
+  Главная функция программы
 
+  Вызывает generateAndSaveSequence с названием сохраняемого файла "sequence_cpp.txt"
+*/
 int main() {
     const std::string filename = "sequence_cpp.txt"; 
 

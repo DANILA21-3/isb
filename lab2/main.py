8b1e53225df6177f831f1d64a72c5cdef4e8abf3
DANILA21-3 <cherdanila184@gmail.com>
2025-05-14 09:04:26 +0400

    Update main.py

--- lab2/main.py
+++ lab2/main.py
@@ -1,11 +1,14 @@
 from tests import *
 from read_write_function import read_file, write_to_file
 
-def calculate_p(sequence: str)->float:
+def calculate_p(sequence: str, config)->float:
 
-    p_1 = pobit_test(sequence)
+    p_1 = pobit_test(sequence, config["sqrt_2"])
     p_2 = test_on_repeat_bit(sequence)
-    p_3 = test_with_blocks(sequence)
+    p_3 = test_with_blocks(sequence, config["M"], pi = np.array([config["pi_1"], 
+                                                                 config["pi_2"], 
+                                                                 config["pi_3"], 
+                                                                 config["pi_4"]]))
     
     result = f"sequence:{sequence}\nP-значение 1: {p_1}\nP-значение 2: {p_2}\nP-значение 3: {p_3}\n\n"
 
@@ -15,6 +18,7 @@ def main():
     try:
         config = read_file('config.json','json')
         cfg = config["Directory"]
+        config_value = config["Const"]
 
         sequence_cpp = read_file(cfg['sequence_cpp'], 'text') 
         print(f"cpp_generator: {sequence_cpp}")
@@ -22,8 +26,8 @@ def main():
         sequence_java = read_file(cfg['sequence_java'], 'text') 
         print(f"java_generator: {sequence_java}")
 
-        result_cpp = calculate_p(sequence_cpp)
-        result_java = calculate_p(sequence_java)
+        result_cpp = calculate_p(sequence_cpp, config_value)
+        result_java = calculate_p(sequence_java, config_value)
         result = result_cpp + result_java
 
         write_to_file(cfg['P-values'], result, 'text')
